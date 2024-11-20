06f45c380e2a99f7cf9516dbd63cb57e5402d5e8
daana <126dpelaezayala@frhsd.com>
2024-11-20 12:44:44 -0500

    Video 3

---
+++ js/sandbo.x.js
@@ -0,0 +1,7 @@
+var titles = document.getElementsByClassName("");
+
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles))):
+
+// titles.forEach(function(item)){
+    // console.log(item):
