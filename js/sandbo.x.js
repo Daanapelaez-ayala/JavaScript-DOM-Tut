6f05a5ddd9dd9c92f00fcd59fc4df5c52d82107d
daana <126dpelaezayala@frhsd.com>
2024-11-21 13:30:33 -0500

    video 7

--- js/sandbo.x.js
+++ js/sandbo.x.js
@@ -8,11 +8,14 @@
 
 //     Const wrap =document.querySelector('#wrapper');
 //     console.log(wrap)
-const banner = document.querySelector('#page-banner'):
+// const banner = document.querySelector('#page-banner'):
  
-console.log('#page-banner node type is:'):
-console.log('#page-banner node name is:', banner.nodeType):
-console.log('#page-banner has child nodes:', banner.hasChildNodes()):
+// console.log('#page-banner node type is:'):
+// console.log('#page-banner node name is:', banner.nodeType):
+// console.log('#page-banner has child nodes:', banner.hasChildNodes()):
 
-const clonedBanner = banner.cloneNode(true):
-console.log(clonedBanner)
+// const clonedBanner = banner.cloneNode(true):
+// console.log(clonedBanner)
+
+const bookList = document.querySelector('#book-list'):
+console.log('the parent node is:'. bookList.parentNode);
