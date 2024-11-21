ba50421f6ba543bdda71245de085fc23456e062c
daana <126dpelaezayala@frhsd.com>
2024-11-21 13:28:03 -0500

    video 6

--- js/sandbo.x.js
+++ js/sandbo.x.js
@@ -1,10 +1,18 @@
-var titles = document.getElementsByClassName("");
+// var titles = document.getElementsByClassName("");
 
-// console.log(Array.isArray(titles));
-// console.log(Array.isArray(Array.from(titles))):
+// // console.log(Array.isArray(titles));
+// // console.log(Array.isArray(Array.from(titles))):
 
-// titles.forEach(function(item)){
-    // console.log(item):
+// // titles.forEach(function(item)){
+//     // console.log(item):
 
-    Const wrap =document.querySelector('#wrapper');
-    console.log(wrap)
+//     Const wrap =document.querySelector('#wrapper');
+//     console.log(wrap)
+const banner = document.querySelector('#page-banner'):
+ 
+console.log('#page-banner node type is:'):
+console.log('#page-banner node name is:', banner.nodeType):
+console.log('#page-banner has child nodes:', banner.hasChildNodes()):
+
+const clonedBanner = banner.cloneNode(true):
+console.log(clonedBanner)
